ca071e42bba6473956cfca03c05fe5c14deacecb
3670156 <3670156@ppti-14-305-07.ufr-info-p6.jussieu.fr>
2019-04-17 19:30:10 +0200

    nimporte quoi

--- src/tests/testAddMess.java
+++ src/tests/testAddMess.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 import Services.Message;
@@ -8,9 +10,8 @@ public class testAddMess {
 	@Test
 	public void test()
 	{
-		System.out.println(Message.addMessage("rebecca", "8D1B17A2FE1222004BE0B9899F720631", "je suis tres heureuse d'etre ici"));
-		System.out.println(Message.addMessage("rebecca", "8D1B17A2FE1222004BE0B9899F720631", "parmis vous ce soir"));
-		System.out.println(Message.addMessage("rebecca", "8D1B17A2FE1222004BE0B9899F720631", "pour la 80 eme ceremonie des hunger games"));
+		System.out.println(Message.addMessage("rebecca", "187C119D3286999D01AB7D8867912432", "je suis tres heureuse d'etre ici"));
 		
+
 	}
 }
